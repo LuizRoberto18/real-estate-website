3421e987f0d0877d623d0298c3bc6a25c4d565b1
Luiz Roberto <luiz.santos@carajas.net.br>
2023-03-04 23:27:41 -0300

    criando a sessão de contato, incrições e rodape

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,75 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-const scrollHeader = () =>{
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
+const scrollHeader = () => {
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50 ? header.classList.add('scroll-header')
+    : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader);
 /*=============== SWIPER POPULAR ===============*/
 var swiperPopular = new Swiper(".popular__container", {
-    spaceBetween: 32,
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item')
 
+accordionItems.forEach((item) => {
+  const accoordionHeader = item.querySelector('.value__accordion-header')
 
+  accoordionHeader.addEventListener('click', () => {
+    const openItem = document.querySelector('.accordion-open')
+
+    toggleItem(item)
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem)
+    }
+  })
+})
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector('.value__accordion-content')
+
+  if (item.classList.contains('accordion-open')) {
+    accordionContent.removeAttribute('style')
+    item.classList.remove('accordion-open')
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+  }
+}
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll('section[id]')
 
-/*=============== SHOW SCROLL UP ===============*/ 
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id')
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.toggle('active-link', (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight))
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
+/*=============== SHOW SCROLL UP ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
